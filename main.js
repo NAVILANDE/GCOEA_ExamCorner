0b3f908bf98a5bff2467e6a6e3e97069ec98030c
vaishnavi <landevaishnavi342@gmail.com>
2024-02-22 00:03:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function navigateToBranch(branch) {
+    window.location.href = `./branches/${branch}.html`;
+}
+
+function navigateToExam(examType) {
+    window.location.href = `../exams/${examType}.html`;
+}
+
+function redirectToExternalLink(link) {
+    window.location.href = link;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html","./branches/*.html","./exams/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
